109cb1e34a4b8de2a1dbc2e9528b3a4f92def850
nokranger <anusorn.thavornporn@gmail.com>
2019-03-31 01:51:45 +0700

    components css(scoped)

--- src/Home.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1>I'm home!</h1>
-</template>
---
+++ src/views/Nok.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>List of Ranger</h1>
+    <ul>
+      <li v-for="(rangers, index) in ranger" :key="index">
+        {{ index }}.{{ rangers }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ranger: ["Nok", "Boom", "Black"]
+    };
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  color: green;
+}
+</style>
