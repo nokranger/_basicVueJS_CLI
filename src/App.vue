3a28c73815d4f33bcaafb93cd8d252f9b9239e86
nokranger <anusorn.thavornporn@gmail.com>
2019-04-01 22:19:33 +0700

    input binding

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <template>
   <div>
-    <keep-alive>
+    <add-blog></add-blog>
+    <!-- <keep-alive>
     <component v-bind:is="component"></component>
     </keep-alive>
     <button v-on:click="component = 'form-one'">Show form one</button>
-    <button v-on:click="component = 'form-two'">Show form Two</button>
+    <button v-on:click="component = 'form-two'">Show form Two</button> -->
   </div>
 </template>
 
 <script>
 import formOne from './views/formOne.vue'
 import formTwo from './views/formTwo.vue'
+import AddBlog from './views/AddBlog.vue'
 
 export default {
   components: {
     'form-one': formOne,
-    'form-two': formTwo
+    'form-two': formTwo,
+    'add-blog': AddBlog
   },
   data() {
     return {
-      component: 'form-two'
+      component: 'form-two',
     };
   },
   methods: {
---
+++ src/views/AddBlog.vue
@@ -0,0 +1,59 @@
+<template>
+    <div id="add-blog">
+        <h1>Add a New Blog Post</h1>
+        <form action="">
+            <label for="">Blog title : </label>
+            <input type="text" v-model.lazy="blog.title" required><br>
+            <label for="">Blog Content</label><br>
+            <textarea v-model.lazy="blog.content"></textarea>
+        </form>
+        <div id="preview">
+            <h1>Preview Blog</h1>
+            <p>Blog Title : {{ blog.title }}</p>
+            <p>Blog Content</p>
+            <p>{{ blog.content }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            blog: {
+                title: "",
+                content: ""
+            }
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style scoped>
+#add-blog *{
+  box-sizing: border-box;  
+}
+#add-blog {
+  margin: 20px auto;
+  max-width: 500;
+}
+label {
+  display: block;  
+}
+input[type="text"],textarea {
+  display: block;
+  width: 100%;  
+}
+#preview {
+  padding: 10px 20px;
+  border: 1px dotted #ccc;
+  margin: 30px;
+}
+h3 {
+    margin-top: 10px;
+}
+</style>
+
