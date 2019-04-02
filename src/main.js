a3ac7f8432fc0aff951c96eb154de1b3f00a15a6
nokranger <anusorn.thavornporn@gmail.com>
2019-04-02 16:40:19 +0700

    registering things locally

--- src/main.js
+++ src/main.js
@@ -9,34 +9,34 @@ Vue.config.productionTip = false;
 Vue.use(VueResource);
 
 //custom directives
-Vue.directive('rainbow', {
-  bind(el, binding, vnode){
-    el.style.color = "#" + Math.random().toString().slice(2, 8);
-  }
-})
+// Vue.directive('rainbow', {
+//   bind(el, binding, vnode){
+//     el.style.color = "#" + Math.random().toString().slice(2, 8);
+//   }
+// })
 
-Vue.directive('theme', {
-  bind(el, binding, vnode){
-    if(binding.value == 'wide'){
-      el.style.maxWidth = "1200px";
-    }else if(binding.value == 'narrow'){
-      el.style.maxWidth = "560px";
-    }
-    if(binding.arg == 'column'){
-      el.style.background = '#ddd';
-      el.style.padding = '20px';
-    }
-  }
-})
+// Vue.directive('theme', {
+//   bind(el, binding, vnode){
+//     if(binding.value == 'wide'){
+//       el.style.maxWidth = "1200px";
+//     }else if(binding.value == 'narrow'){
+//       el.style.maxWidth = "560px";
+//     }
+//     if(binding.arg == 'column'){
+//       el.style.background = '#ddd';
+//       el.style.padding = '20px';
+//     }
+//   }
+// })
 
 //Filters
-Vue.filter('to-uppercase', function(value){
-  return value.toUpperCase()
-})
+// Vue.filter('to-uppercase', function(value){
+//   return value.toUpperCase()
+// })
 
-Vue.filter('snippet', function(value){
-  return value.slice(0,100) + ' ...'
-})
+// Vue.filter('snippet', function(value){
+//   return value.slice(0,100) + ' ...'
+// })
 
 new Vue({
   router,
--- src/views/showBlog.vue
+++ src/views/showBlog.vue
@@ -34,6 +34,38 @@ export default {
         return blog.title.match(this.search);
       });
     }
+  },
+  filters: {
+    'to-uppercase': function(value) {
+      return value.toUpperCase();
+    },
+    'snippet': function(value){
+      return value.slice(0,100) + ' ...'  
+    }
+  },
+  directives: {
+    rainbow: {
+      bind(el, binding, vnode) {
+        el.style.color =
+          "#" +
+          Math.random()
+            .toString(16)
+            .slice(2, 8);
+      }
+    },
+    theme: {
+      bind(el, binding, vnode) {
+        if (binding.value == "wide") {
+          el.style.maxWidth = "1200px";
+        } else if (binding.value == "narrow") {
+          el.style.maxWidth = "560px";
+        }
+        if (binding.arg == "column") {
+          el.style.background = "#ddd";
+          el.style.padding = "20px";
+        }
+      }
+    }
   }
 };
 </script>
